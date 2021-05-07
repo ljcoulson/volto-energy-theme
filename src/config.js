cbd7c3ab5553b25009a644be934ecbf6089a9473
nileshgulia1 <nileshgulia@gmail.com>
2021-05-07 22:52:07 +0530

    try to make it work without volto-addons

--- src/config.js
+++ src/config.js
@@ -1,34 +1,34 @@
 import TokenWidget from '@plone/volto/components/manage/Widgets/TokenWidget';
-import chartIcon from '@plone/volto/icons/world.svg';
+//import chartIcon from '@plone/volto/icons/world.svg';
 import TopicsView from './components/theme/View/TopicsView';
 import TopicsTabView from './components/theme/View/TopicsTabView';
-import FolderListingBlockView from 'volto-addons/FolderListing/BlockView';
-import FolderListingBlockEdit from 'volto-addons/FolderListing/BlockEdit';
+// import FolderListingBlockView from 'volto-addons/FolderListing/BlockView';
+// import FolderListingBlockEdit from 'volto-addons/FolderListing/BlockEdit';
 
-import HiddenWidget from 'volto-addons/Widgets/Hidden';
-import CollectionYears from 'volto-addons/Widgets/CollectionYears';
-import PickObject from 'volto-addons/PickObject';
-import ObjectListWidget from 'volto-addons/Widgets/ObjectList';
-import AlignBlockWidget from 'volto-addons/Widgets/Align';
-import AttachedImageWidget from 'volto-addons/Widgets/AttachedImage';
-import TemplatingToolbarWidget from 'volto-addons/Widgets/TemplatingToolbar';
+// import HiddenWidget from 'volto-addons/Widgets/Hidden';
+// import CollectionYears from 'volto-addons/Widgets/CollectionYears';
+// import PickObject from 'volto-addons/PickObject';
+// import ObjectListWidget from 'volto-addons/Widgets/ObjectList';
+// import AlignBlockWidget from 'volto-addons/Widgets/Align';
+// import AttachedImageWidget from 'volto-addons/Widgets/AttachedImage';
+// import TemplatingToolbarWidget from 'volto-addons/Widgets/TemplatingToolbar';
 
-import MapView from 'volto-addons/Map/View';
+// import MapView from 'volto-addons/Map/View';
 
-import WebMapBlockView from 'volto-addons/WebMap/BlockView';
-import WebMapBlockEdit from 'volto-addons/WebMap/BlockEdit';
+// import WebMapBlockView from 'volto-addons/WebMap/BlockView';
+// import WebMapBlockEdit from 'volto-addons/WebMap/BlockEdit';
 
-import ConnectedMapView from 'volto-addons/ConnectedMap/BlockView';
-import ConnectedMapEdit from 'volto-addons/ConnectedMap/BlockEdit';
+// import ConnectedMapView from 'volto-addons/ConnectedMap/BlockView';
+// import ConnectedMapEdit from 'volto-addons/ConnectedMap/BlockEdit';
 
-import ConnectedControl from 'volto-addons/ConnectedControl/ConnectedControl';
-// import SearchEdit from 'volto-addons/SearchBlock/Edit';
-// import SearchView from 'volto-addons/SearchBlock/View';
+// import ConnectedControl from 'volto-addons/ConnectedControl/ConnectedControl';
+// // import SearchEdit from 'volto-addons/SearchBlock/Edit';
+// // import SearchView from 'volto-addons/SearchBlock/View';
 
-import TableEdit from 'volto-addons/TableBlock/Edit';
-import TableView from 'volto-addons/TableBlock/View';
+// import TableEdit from 'volto-addons/TableBlock/Edit';
+// import TableView from 'volto-addons/TableBlock/View';
 
-import * as remoteAddonReducers from 'volto-addons/reducers';
+// import * as remoteAddonReducers from 'volto-addons/reducers';
 
 import '@plone/volto/config';
 
@@ -88,8 +88,8 @@ export default function applyConfig(config) {
     },
     contentTypesViews: {
       ...config.views.contentTypesViews,
-      EmbeddedMap: MapView,
-      embeddedmap: MapView,
+      // EmbeddedMap: MapView,
+      // embeddedmap: MapView,
     },
   };
 
@@ -102,29 +102,29 @@ export default function applyConfig(config) {
     },
     id: {
       ...config.widgets.id,
-      collection_years: CollectionYears,
-      blocks: HiddenWidget,
-      blocks_layout: HiddenWidget,
-      templatingtoolbar: TemplatingToolbarWidget,
+      // collection_years: CollectionYears,
+      // blocks: HiddenWidget,
+      // blocks_layout: HiddenWidget,
+      // templatingtoolbar: TemplatingToolbarWidget,
     },
     widget: {
       ...config.widgets.widget,
-      sidebar: [TemplatingToolbarWidget],
-      object_by_path: PickObject,
-      objectlist: ObjectListWidget,
-      align: AlignBlockWidget,
-      attachedimage: AttachedImageWidget,
+      // sidebar: [TemplatingToolbarWidget],
+      // object_by_path: PickObject,
+      // objectlist: ObjectListWidget,
+      // align: AlignBlockWidget,
+      // attachedimage: AttachedImageWidget,
     },
   };
 
-  config.blocks.blocksConfig.folder_contents_block = {
-    id: 'folder_contents_block',
-    title: 'Folder Contents',
-    view: FolderListingBlockView,
-    edit: FolderListingBlockEdit,
-    icon: chartIcon,
-    group: 'custom_addons',
-  };
+  // config.blocks.blocksConfig.folder_contents_block = {
+  //   id: 'folder_contents_block',
+  //   title: 'Folder Contents',
+  //   view: FolderListingBlockView,
+  //   edit: FolderListingBlockEdit,
+  //   icon: chartIcon,
+  //   group: 'custom_addons',
+  // };
 
   //Custom Search block
   // blocks.blocksConfig.search_block = {
@@ -136,46 +136,46 @@ export default function applyConfig(config) {
   //   group: 'custom_addons',
   // };
 
-  config.blocks.blocksConfig.table_block = {
-    id: 'table_block',
-    title: 'Table block',
-    view: TableView,
-    edit: TableEdit,
-    icon: chartIcon,
-    group: 'custom_addons',
-  };
-  config.blocks.blocksConfig.connected_map = {
-    id: 'connected_map',
-    title: 'Connected Map',
-    view: ConnectedMapView,
-    edit: ConnectedMapEdit,
-    icon: chartIcon,
-    group: 'custom_addons',
-  };
+  // config.blocks.blocksConfig.table_block = {
+  //   id: 'table_block',
+  //   title: 'Table block',
+  //   view: TableView,
+  //   edit: TableEdit,
+  //   icon: chartIcon,
+  //   group: 'custom_addons',
+  // };
+  // config.blocks.blocksConfig.connected_map = {
+  //   id: 'connected_map',
+  //   title: 'Connected Map',
+  //   view: ConnectedMapView,
+  //   edit: ConnectedMapEdit,
+  //   icon: chartIcon,
+  //   group: 'custom_addons',
+  // };
 
-  config.blocks.blocksConfig.connected_control = {
-    id: 'connected_control',
-    title: 'Connected Control',
-    view: ConnectedControl,
-    edit: ConnectedControl,
-    icon: chartIcon,
-    group: 'custom_addons',
-  };
+  // config.blocks.blocksConfig.connected_control = {
+  //   id: 'connected_control',
+  //   title: 'Connected Control',
+  //   view: ConnectedControl,
+  //   edit: ConnectedControl,
+  //   icon: chartIcon,
+  //   group: 'custom_addons',
+  // };
 
-  config.blocks.blocksConfig.web_map = {
-    id: 'web_map',
-    title: 'Web Map',
-    view: WebMapBlockView,
-    edit: WebMapBlockEdit,
-    icon: chartIcon,
-    group: 'custom_addons',
-    sidebarTab: 1,
-  };
+  // config.blocks.blocksConfig.web_map = {
+  //   id: 'web_map',
+  //   title: 'Web Map',
+  //   view: WebMapBlockView,
+  //   edit: WebMapBlockEdit,
+  //   icon: chartIcon,
+  //   group: 'custom_addons',
+  //   sidebarTab: 1,
+  // };
 
   config.addonRoutes = [...config.addonRoutes];
   config.addonReducers = {
     ...config.addonReducers,
-    ...remoteAddonReducers,
+    // ...remoteAddonReducers,
   };
   return config;
 }
